6c6ca1a6b1c44566a35340d15818ee18cbb52e17
Alexei Gousev <numegil@gmail.com>
2012-07-04 17:31:44 -0700

    Updated to Django 1.4 messages

--- avatar_crop/views.py
+++ avatar_crop/views.py
@@ -7,8 +7,9 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
-from django.utils.translation import ugettext
+from django.utils.translation import ugettext as _
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 try:
     from cStringIO import StringIO
@@ -77,8 +78,7 @@ def avatar_crop(request, id=None):
             Avatar.objects.filter(id=avatar.id).update(primary=False)
             new_avatar = Avatar(user=request.user, primary=True, avatar=thumb)
             new_avatar.save()
-            request.user.message_set.create(
-                message=ugettext("Your new avatar has been saved successfully."))
+            messages.add_message(request, messages.INFO, _("Your new avatar has been saved successfully."))
             return HttpResponseRedirect(reverse("avatar_change"))
 
     return render_to_response("avatar_crop/crop.html", {
